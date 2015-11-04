bbc6edf9612a936c0d38c99e2285f5d8148f7a77
FiftyEight <FiftyEight>
2015-11-04 12:36:54 -0500

    added sql evolution to git so it shows up on heroku

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,30 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table student_member (
+  first_name                varchar(255),
+  last_name                 varchar(255),
+  major                     varchar(255),
+  second_major              varchar(255),
+  email                     varchar(255),
+  class_level               varchar(255),
+  grad_year                 varchar(255),
+  password                  varchar(255),
+  pid                       varchar(255),
+  home_town                 varchar(255),
+  id                        bigint)
+;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists student_member;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
