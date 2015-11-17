57f1fa5fda4168a75b44f8a70e52faafe5a8460d
Jared Layne <jlayne@live.unc.edu>
2015-11-16 22:48:20 -0500

    compiled for jdk 1.7 hopefully

---
+++ target/scala-2.11/src_managed/main/controllers/routes.java
@@ -0,0 +1,23 @@
+// @SOURCE:C:/Users/Jared/Documents/CampusY/play17/conf/routes
+// @HASH:35cc300075f0193e6cddbcfb63f508610f0c266c
+// @DATE:Mon Nov 16 22:46:21 EST 2015
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+
+public static class javascript {
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+}
+          
+
+public static class ref {
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+}
+          
+}
+          
